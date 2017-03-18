b09658aca1cd7b8e0471994a1633118335d8a60b
rogergcc <rogergcc@outlook.com>
2017-03-18 01:08:26 -0500

    Mensj. ya muestra el porcentaje, falta desbahiblitar la edicion de los textbox de votos

--- Encuesta_Colquehuanca/Encuesta.aspx.cs
+++ Encuesta_Colquehuanca/Encuesta.aspx.cs
@@ -22,6 +22,16 @@ namespace Encuesta_Colquehuanca
                 txtVotosWeb.Text = "0";
                 txtVotosBaseDatos.Text = "0";
                 txtVotosIngles.Text = "0";
+
+                txtVotosRedes.Enabled = false;
+                txtVotosWeb.Enabled = false;
+                txtVotosBaseDatos.Enabled = false;
+                txtVotosIngles.Enabled = false;
+
+                txtPorcentajeRedes.Text = "0";
+                txtPorcentajeWeb.Text = "0";
+                txtPorcentajeBaseDatos.Text = "0";
+                txtPorcentajeIngles.Text = "0";
             }
             
         }
@@ -45,14 +55,20 @@ namespace Encuesta_Colquehuanca
             {
                 txtVotosIngles.Text = (Int32.Parse(txtVotosIngles.Text) + 1).ToString();
             }
-            txtTotalVotos.Text = (Int32.Parse(txtVotosRedes.Text) + Int32.Parse(txtVotosWeb.Text) + Int32.Parse(txtVotosBaseDatos.Text) + Int32.Parse(txtVotosIngles.Text)).ToString();
+            txtTotalVotos.Text = (Int32.Parse(txtVotosRedes.Text) + 
+                                    Int32.Parse(txtVotosWeb.Text) + 
+                                    Int32.Parse(txtVotosBaseDatos.Text) + 
+                                    Int32.Parse(txtVotosIngles.Text)).ToString();
 
-            txtPorcentajeRedes.Text = ((float.Parse(txtVotosRedes.Text) * 100f) / float.Parse(txtTotalVotos.Text)).ToString();
-            txtPorcentajeWeb.Text = ((float.Parse(txtVotosWeb.Text) * 100f) / float.Parse(txtTotalVotos.Text)).ToString();
-            txtPorcentajeBaseDatos.Text = ((float.Parse(txtVotosBaseDatos.Text) * 100f) / float.Parse(txtTotalVotos.Text)).ToString();
-            txtPorcentajeIngles.Text = ((float.Parse(txtVotosIngles.Text) * 100f) / float.Parse(txtTotalVotos.Text)).ToString();
-            
-            txtPorcentajeTotal.Text = (Int32.Parse(txtPorcentajeRedes.Text) + Int32.Parse(txtPorcentajeWeb.Text) + Int32.Parse(txtPorcentajeBaseDatos.Text) + Int32.Parse(txtPorcentajeIngles.Text)).ToString();
+            txtPorcentajeRedes.Text = ((float.Parse(txtVotosRedes.Text) * 100f) / float.Parse(txtTotalVotos.Text)).ToString("0.00");
+            txtPorcentajeWeb.Text = ((float.Parse(txtVotosWeb.Text) * 100f) / float.Parse(txtTotalVotos.Text)).ToString("0.00");
+            txtPorcentajeBaseDatos.Text = ((float.Parse(txtVotosBaseDatos.Text) * 100f) / float.Parse(txtTotalVotos.Text)).ToString("0.00");
+            txtPorcentajeIngles.Text = ((float.Parse(txtVotosIngles.Text) * 100f) / float.Parse(txtTotalVotos.Text)).ToString("0.00");
+
+            txtPorcentajeTotal.Text = (float.Parse(txtPorcentajeRedes.Text) + 
+                                        float.Parse(txtPorcentajeWeb.Text) + 
+                                        float.Parse(txtPorcentajeBaseDatos.Text) +
+                                        float.Parse(txtPorcentajeIngles.Text)).ToString("0");
         }
     }
 }
